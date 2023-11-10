2f2c7a68516412d47e38f7ee3f638775190bf434
邓林杰 <756492215@qq.com>
2023-11-10 15:40:54 +0800

    修改枚举

--- nkk-kit-web/src/main/java/org/nkk/web/conf/json/JacksonConfig.java
+++ nkk-kit-web/src/main/java/org/nkk/web/conf/json/JacksonConfig.java
@@ -1,10 +1,11 @@
 package org.nkk.web.conf.json;
 
+import org.nkk.core.enums.common.BaseEnum;
+import org.nkk.web.conf.serializer.JacksonEnumSerializer;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
 import org.springframework.boot.autoconfigure.jackson.JacksonAutoConfiguration;
 import org.springframework.context.annotation.Bean;
-
 
 @AutoConfigureBefore(JacksonAutoConfiguration.class)
 public class JacksonConfig {
@@ -12,8 +13,12 @@ public class JacksonConfig {
     @Bean
     public Jackson2ObjectMapperBuilderCustomizer customizer() {
         return builder -> {
-            builder.modules(new CustomModule());
+           //builder.deserializerByType(Enum.class, new JacksonEnumDeserializer());
+            builder.serializerByType(BaseEnum.class, new JacksonEnumSerializer());
         };
     }
+
+
+
 }
 
---
+++ nkk-kit-web/src/main/java/org/nkk/web/conf/serializer/JacksonEnumSerializer.java
@@ -0,0 +1,29 @@
+package org.nkk.web.conf.serializer;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import org.nkk.core.enums.common.BaseEnum;
+import org.springframework.boot.jackson.JsonComponent;
+
+import java.io.IOException;
+
+
+/**
+ * <p>描述: 枚举{@link BaseEnum} 的jackson序列化实现
+ * <p>开发者: dlj 
+ * <p>时间: 2022/1/27 13:56
+ **/
+@JsonComponent
+public class JacksonEnumSerializer extends JsonSerializer<BaseEnum> {
+
+    @Override
+    public void serialize(BaseEnum baseEnum, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+        gen.writeObject(baseEnum.getEnumResp());
+    }
+
+    @Override
+    public Class<BaseEnum> handledType() {
+        return BaseEnum.class;
+    }
+}
