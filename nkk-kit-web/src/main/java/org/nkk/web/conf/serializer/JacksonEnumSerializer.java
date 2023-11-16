bc51875752787cede349102439da5e5a2c3f67e7
邓林杰 <756492215@qq.com>
2023-11-16 14:04:50 +0800

    修改枚舉序列化

--- nkk-kit-web/src/main/java/org/nkk/web/conf/serializer/JacksonEnumSerializer.java
+++ nkk-kit-web/src/main/java/org/nkk/web/conf/serializer/JacksonEnumSerializer.java
@@ -1,16 +1,16 @@
 package org.nkk.web.conf.serializer;
 
+import cn.hutool.core.util.ReflectUtil;
+import com.fasterxml.jackson.annotation.JsonValue;
 import com.fasterxml.jackson.core.JsonGenerator;
-import com.fasterxml.jackson.databind.BeanProperty;
-import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.fasterxml.jackson.databind.ser.ContextualSerializer;
 import lombok.NoArgsConstructor;
 import org.nkk.core.enums.common.BaseEnum;
 import org.springframework.boot.jackson.JsonComponent;
 
 import java.io.IOException;
+import java.lang.reflect.Field;
 
 
 /**
@@ -24,7 +24,14 @@ public class JacksonEnumSerializer extends JsonSerializer<BaseEnum> {
 
     @Override
     public void serialize(BaseEnum baseEnum, JsonGenerator gen, SerializerProvider serializers) throws IOException {
-        System.out.println("枚举序列化。。");
+        Field[] fields = ReflectUtil.getFieldsDirectly(baseEnum.getClass(),false);
+        for (Field field : fields) {
+            JsonValue annotation = field.getAnnotation(JsonValue.class);
+            if(annotation != null) {
+                gen.writeObject(ReflectUtil.getFieldValue(baseEnum,field.getName()));
+                return;
+            }
+        }
         gen.writeObject(baseEnum.getEnumResp());
     }
 
